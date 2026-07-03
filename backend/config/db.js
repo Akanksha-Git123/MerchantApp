eecb302843b7ba18aae59dad350c7d5f31ef6515
W2-89710-Akanksha <akankshachinake19@gmail.com>
2026-07-03 15:52:26 +0530

    Fix Aiven SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,9 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
 });
 
